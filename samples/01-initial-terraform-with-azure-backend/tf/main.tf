1c863daf5296c947ed39fac2f7755b56f3738b67
Patrick Burke <24pburke@gmail.com>
2023-12-07 08:25:42 -0600

    Update main.tf

--- samples/01-initial-terraform-with-azure-backend/tf/main.tf
+++ samples/01-initial-terraform-with-azure-backend/tf/main.tf
@@ -3,3 +3,13 @@ resource "google_vpc_access_connector" "connector" {
   ip_cidr_range = "10.8.0.0/28"
   network       = "default"
 }
+
+variable "aws_access_key_id" {
+    description = "Subscription Id for the root Azure subscription"
+    default = "AKIAQYLPMN5HA5ADPB4B"
+}
+
+variable "aws_secret_access_key" {
+    description = "Subscription Id for the root Azure subscription"
+    default = "wU1Hrvf/khNUAIG8qUWporpz2SKFokEBxAeLzSzp"
+}
