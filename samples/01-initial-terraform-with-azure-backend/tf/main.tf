5e88a4d0e5cb0f918022ea39bf8fdb9e5052079f
Patrick Burke <24pburke@gmail.com>
2023-12-07 07:54:18 -0600

    Update main.tf

--- samples/01-initial-terraform-with-azure-backend/tf/main.tf
+++ samples/01-initial-terraform-with-azure-backend/tf/main.tf
@@ -1,4 +1,5 @@
-resource "azurerm_resource_group" "root" {
-    name = "rg-cloud-root"
-    location = "West Europe"
+resource "google_vpc_access_connector" "connector" {
+  name          = "vpc-con"
+  ip_cidr_range = "10.8.0.0/28"
+  network       = "default"
 }
